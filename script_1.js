e474348a692179f3669a48b564dd81ca44b363cf
Rajasthanidon <nirdoshmeena350@gmail.com>
2026-04-26 20:22:59 +0530

    update

---
+++ script_1.js
@@ -0,0 +1,16 @@
+let size = 25;
+let size1 = 25;
+function font_size(){
+    size = size + 4;
+    size1 = size1 -4; 
+    document.getElementById("ha").style.fontSize = size + "px";
+    document.getElementById("cute_emoji").style.height = size + "px";
+    
+    if(size1>=4){
+    document.getElementById("nahi").style.fontSize = size1 + "px";
+    }
+    else{
+        document.getElementById("nahi").disabled = true;
+    }
+
+}
